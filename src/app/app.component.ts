d2b8de5073ce9f7feb88d914cbba821c3229ce6e
ajmathis1231 <ajmathis1231@gmail.com>
2024-08-07 21:54:28 -0500

    Initialization, created map and detail comp, inserted map, details table.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './map/map.component';
+import { DetailsComponent } from './details/details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MapComponent, DetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
